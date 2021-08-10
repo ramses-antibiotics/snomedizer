66400f51f0714c753837fe710b42f547af474cbe
Peter Dutey-Magni <sejjpfd@ucl.ac.uk>
2021-08-10 16:53:54 +0100

    draft vignette until named entity recognition

--- data-raw/vignette_indications_concepts.R
+++ data-raw/vignette_indications_concepts.R
@@ -1,5 +1,6 @@
 
 library(snomedizer)
+library(dplyr)
 snomedizer_options_set(
   endpoint = "https://snowstorm.ihtsdotools.org/snowstorm/snomed-ct",
   branch = "MAIN/2021-07-31",
@@ -11,9 +12,11 @@ abx_indications_concepts <- concepts_descendants(
                  "128045006 | Cellulitis (disorder) |",
                  "128477000 | Abscess (disorder) |")
 ) %>%
-  bind_rows() %>%
+  dplyr::bind_rows() %>%
   unique()
 
-abx_indications_descriptions <- concepts_descriptions(
-  abx_indications_concepts$conceptId
-)
+abx_indications_descriptions <- abx_indications_concepts$conceptId %>%
+  concepts_descriptions() %>%
+  dplyr::bind_rows()
+
+usethis::use_data(abx_indications_concepts, abx_indications_descriptions, internal = TRUE, overwrite = TRUE)
